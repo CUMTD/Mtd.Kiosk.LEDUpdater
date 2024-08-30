9ce5e25adf48431190ebd67f12b18ad5055e03cc
matt novelli <mattnovelli@gmail.com>
2024-08-30 10:17:59 -0500

    Bump v1.0.6 (#5)
    * update devkiosk field name
    
    * updates for new package

--- Service/LEDUpdaterService.cs
+++ Service/LEDUpdaterService.cs
@@ -1,7 +1,8 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Mtd.Kiosk.LedUpdater.IpDisplaysApi;
+using Mtd.Kiosk.IpDisplaysApi;
+using Mtd.Kiosk.IpDisplaysApi.Models;
 using Mtd.Kiosk.LedUpdater.Realtime;
 using Mtd.Kiosk.LedUpdater.Realtime.Entitites;
 using Mtd.Kiosk.LedUpdater.SanityClient.Schema;
@@ -46,7 +47,8 @@ internal class LedUpdaterService : BackgroundService, IDisposable
 		// create a sign client for each IP address
 		foreach (var kiosk in kiosks)
 		{
-			_signs.Add(kiosk.Id, new LedSign(_ipDisplaysAPIClientFactory.CreateClient(kiosk.LedIp), _logger));
+			// todo: this kinda funky with all those kiosk.Ids
+			_signs.Add(kiosk.Id, new LedSign(kiosk.Id, _ipDisplaysAPIClientFactory.CreateClient(kiosk.Id, kiosk.LedIp), _logger));
 
 			// fill this kiosk's departures stack
 			var departures = await FetchDepartures(kiosk, stoppingToken);
--- Service/Program.cs
+++ Service/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Mtd.Kiosk.LedUpdater.IpDisplaysApi;
+using Mtd.Kiosk.IpDisplaysApi;
 using Mtd.Kiosk.LedUpdater.Realtime;
 using Mtd.Kiosk.LedUpdater.SanityClient;
 using Mtd.Kiosk.LedUpdater.Service;
